aae7f70150cddb65bece1706b92ba492fcc1a27b
Tathanh308 <tathanh3082003@gmail.com>
2025-02-28 15:56:11 +0700

    loginFBandGoogle

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,8 @@ use App\Http\Controllers\AuthController;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
 
 // Đăng ký
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register');
@@ -28,3 +29,27 @@ Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
+use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+Route::get('auth/{provider}', function ($provider) {
+    return Socialite::driver($provider)->redirect();
+})->where('provider', 'google|facebook')->name('social.login');
+
+Route::get('auth/{provider}/callback', function ($provider) {
+    $socialUser = Socialite::driver($provider)->user();
+
+    $user = User::updateOrCreate([
+        'email' => $socialUser->getEmail(),
+    ], [
+        'name' => $socialUser->getName(),
+        'social_id' => $socialUser->getId(),
+        'social_provider' => $provider,
+        'password' => bcrypt('password') // Mặc định đặt mật khẩu (có thể thay đổi sau)
+    ]);
+
+    Auth::login($user);
+
+    return redirect()->route('home');
+})->where('provider', 'google|facebook');
