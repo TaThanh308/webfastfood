6f6edf836cf58d0571e04170f38185413050c67f
DangDuong123 <conchoden234@gmail.com>
2025-03-04 16:15:51 +0700

    banner_admin
    và đã hiện thị bên cả khách hàng
    (trưa sửa cột tiêu đề: mong muốn cột tiêu đề không bắt buộc nhập.
    )

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Banner;
+
+class HomeController extends Controller
+{
+    public function index()
+{
+    $banners = Banner::where('status', 'active')->get();
+    return view('home', compact('banners'));
+}
+
+}
--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -8,5 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Banner extends Model
 {
     use HasFactory;
+    
     protected $fillable = ['title', 'image', 'link', 'status'];
 }
+
